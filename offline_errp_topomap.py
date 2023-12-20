ab0e438f895cdd068a0537cc5f7b3f11cc73610a
28208 <770081067@qq.com>
2023-12-20 17:24:09 +0800

    1.0

---
+++ offline_errp_topomap.py
@@ -0,0 +1,142 @@
+import os
+import mne
+import numpy as np
+import matplotlib.pyplot as plt
+
+evoked_correct = []
+evoked_error = []
+n_block = 3
+file_path = "E:\eeg_data\\2023_12"
+for idx_block in range(2, n_block + 1):
+    # Load data
+    data_path = os.path.join(file_path, "1215-{}.cnt".format(idx_block))
+    raw = mne.io.read_raw_cnt(data_path, eog=['HEO', 'VEO'], emg=['EMG'], ecg=['EKG'],
+                              preload=True, verbose=False)
+    raw.drop_channels(['M1', 'M2'])
+    chan_names = raw.info["ch_names"]
+    print(chan_names)
+    # Rereference
+
+    raw.set_eeg_reference(ref_channels='average')
+    # raw.set_eeg_reference(ref_channels=['TP7', 'TP8'])
+    # Bandpass Filter
+    raw = raw.filter(l_freq=1, h_freq=10, method='iir')
+
+    # Epochs
+    events, event_id = mne.events_from_annotations(raw, event_id={'1': 1, '2': 2})
+    epochs = mne.Epochs(raw, events, event_id=[1, 2], tmin=-0.2, tmax=1.0,
+                        baseline=(-0.2, 0),
+                        preload=True,
+                        )
+    epochs = epochs.get_data()
+
+    idx_error = np.where(events[:, -1] == 1)[0]
+    # 错误是1
+    idx_correct = np.where(events[:, -1] == 2)[0]
+    # idx_correct = np.setdiff1d(idx_correct, idx_error+1)
+    # idx_correct = np.setdiff1d(idx_correct, idx_error-1)
+    # idx_correct = np.setdiff1d(idx_correct, [30*i for i in range(12)])
+    epochs_error = epochs[idx_error]
+    epochs_correct = epochs[idx_correct]
+
+    evoked_error.append(np.mean(epochs_error, axis=0))
+    evoked_correct.append(np.mean(epochs_correct, axis=0))
+
+evoked_correct = np.stack(evoked_correct, axis=0)
+evoked_error = np.stack(evoked_error, axis=0)
+
+evoked_correct_mean = np.mean(evoked_correct, axis=0) * 1e6
+evoked_error_mean = np.mean(evoked_error, axis=0) * 1e6
+evoked_differ = evoked_error_mean - evoked_correct_mean
+print(evoked_error_mean.shape)
+# In[]: Time domain
+fig = plt.figure(figsize=(6, 3), tight_layout=True)
+plt.rcParams['font.sans-serif'] = 'Arial'
+plt.rcParams['xtick.direction'] = 'in'
+plt.rcParams['ytick.direction'] = 'in'
+ax = plt.gca()
+ax.spines['right'].set_visible(False)
+ax.spines['top'].set_visible(False)
+
+ylim = 5
+idx_chan_list = [18]
+for idx, idx_chan in enumerate(idx_chan_list):
+    plt.subplot(len(idx_chan_list), 1, idx + 1)
+    x_time = np.linspace(-0.2, 1.0, evoked_correct_mean.shape[1])
+    plt.plot(x_time, evoked_correct_mean[idx_chan],
+             linewidth=2, color=[127 / 255, 203 / 255, 164 / 255], label='Correct')
+    plt.plot(x_time, evoked_error_mean[idx_chan],
+             linewidth=2, color=[164 / 255, 5 / 255, 69 / 255], label='Error')
+    plt.plot(x_time, evoked_differ[idx_chan], linewidth=1, color='b', label='Difference')
+
+    plt.plot([0, 0], [-ylim, ylim], linewidth=0.5, color='grey', linestyle='--')
+    plt.xticks(np.arange(-0.2, 1.6, 0.2), fontsize=14)
+    plt.xlim([-0.2, 1])
+    plt.ylim([-ylim, ylim])
+    plt.yticks(fontsize=14)
+    plt.xlabel('Time (s)', fontsize=16)
+    plt.ylabel('Amplitude (μV)', fontsize=16)
+    plt.text(-0.35, ylim - 1.5, '{}'.format(chan_names[idx_chan]), fontsize=18)
+    plt.legend(loc='upper right', fontsize=8, frameon=False)
+
+# plt.savefig('2.png', dpi=300)
+plt.show()
+
+# In[]:
+# locs文件地址
+locs_info_path = "64-channels.loc"
+
+chan_drop = ['CB1', 'CB2']
+
+idx_chan_drop = [chan_names.index(c) for c in chan_drop]
+evoked_correct_mean2 = np.delete(evoked_correct_mean.copy(), idx_chan_drop, axis=0)
+evoked_error_mean2 = np.delete(evoked_error_mean.copy(), idx_chan_drop, axis=0)
+evoked_differ_2 = evoked_error_mean2 - evoked_correct_mean2
+new_chan_names = chan_names.copy()
+for idx_chan in range(len(chan_drop)):
+    new_chan_names.remove(chan_drop[idx_chan])
+montage = mne.channels.read_custom_montage(locs_info_path)
+info = mne.create_info(ch_names=new_chan_names, sfreq=1000., ch_types='eeg')
+evoked_correct_mean2 = mne.EvokedArray(evoked_correct_mean2, info)
+evoked_correct_mean2.set_montage(montage)
+evoked_error_mean2 = mne.EvokedArray(evoked_error_mean2, info)
+evoked_error_mean2.set_montage(montage)
+evoked_differ_2 = mne.EvokedArray(evoked_differ_2, info)
+evoked_differ_2.set_montage(montage)
+fig, ax = plt.subplots(nrows=5, ncols=10, figsize=(15, 10))
+# 正确的脑地形图
+# for i in range(50):
+#     row_idx = i // 10  # 行索引
+#     col_idx = i % 10  # 列索引
+#
+#     # 使用 ax[row_idx, col_idx] 来指定子图
+#     mne.viz.plot_topomap(evoked_error_mean2.data[:, int(200 + 20 * i)],
+#                          evoked_error_mean2.info, show=False, axes=ax[row_idx, col_idx])
+#
+#     ax[row_idx, col_idx].set_title("{} ms".format(20 * i))
+# fig.suptitle("Error Topomaps", fontsize=16)
+# plt.show()
+# 错误的脑地形图
+# for i in range(50):
+#     row_idx = i // 10  # 行索引
+#     col_idx = i % 10  # 列索引
+#
+#     # 使用 ax[row_idx, col_idx] 来指定子图
+#     mne.viz.plot_topomap(evoked_correct_mean2.data[:, int(200 + 20 * i)],
+#                          evoked_correct_mean2.info, show=False, axes=ax[row_idx, col_idx])
+#
+#     ax[row_idx, col_idx].set_title("{} ms".format(20 * i))
+# fig.suptitle("Correct Topomaps", fontsize=16)
+# plt.show()
+# 差异波的脑地形图
+for i in range(50):
+    row_idx = i // 10  # 行索引
+    col_idx = i % 10  # 列索引
+
+    # 使用 ax[row_idx, col_idx] 来指定子图
+    mne.viz.plot_topomap(evoked_differ_2.data[:, int(200 + 20 * i)],
+                         evoked_differ_2.info, show=False, axes=ax[row_idx, col_idx])
+
+    ax[row_idx, col_idx].set_title("{} ms".format(20 * i))
+fig.suptitle("Differ Topomaps", fontsize=16)
+plt.show()
---
+++ process.py
@@ -0,0 +1,76 @@
+import time
+import airsim
+import random
+# import serial
+def draw(client,i,dir):
+    kinematics_state = client.simGetGroundTruthKinematics()
+    position = kinematics_state.position
+    label=random.random()
+    if label < 0.3:
+        flag=dir[i]
+    else:
+        flag=-dir[i]
+    #1是箭头指向下 -1是箭头向下
+
+    start = airsim.Vector3r(position.x_val+8, position.y_val, position.z_val)
+    direction = airsim.Vector3r(position.x_val+8, position.y_val, position.z_val+2*flag)
+    client.simPlotArrows([start],
+                         [direction],
+                         color_rgba=[1.0, 0.0, 0.0, 1.0], duration=0.5, arrow_size=150, thickness=10)
+    return label
+def draw_mode2(client,i,dir):
+    kinematics_state = client.simGetGroundTruthKinematics()
+    position = kinematics_state.position
+    flag=dir[i]
+    start = airsim.Vector3r(position.x_val + 8, position.y_val, position.z_val)
+    direction = airsim.Vector3r(position.x_val + 8, position.y_val, position.z_val + 2 * flag)
+    client.simPlotArrows([start],
+                         [direction],
+                         color_rgba=[0.0, 1.0, 0.0, 1.0], duration=0.5, arrow_size=150, thickness=10)
+    return direction
+if __name__ == '__main__':
+
+    n=input('select mode')
+    # ser = serial.Serial('com8', 115200, timeout = 0.5)  # winsows系统使用com1口连接串行口
+    #实例化serial
+    # 连接到AirSim
+    client = airsim.MultirotorClient(ip='172.20.10.14')
+    client.reset()
+    client.confirmConnection()
+    # 获取无人机初始状态
+    state = client.getMultirotorState()
+    position = state.kinematics_estimated.position
+    orientation = state.kinematics_estimated.orientation
+    client.enableApiControl(True)
+    client.armDisarm(True)
+    client.moveToZAsync(-40, 15).join()
+    time.sleep(3)
+    dir=[1,-1,-1,1,-1,1,-1,1,-1,1,1,-1,1,1,-1,-1,-1,1,1,-1,1,-1,-1,1,-1,1,-1,1,-1,1,1,-1,1,1,-1,-1,-1,1,1,-1,-1,1,1,-1,1,-1,-1,1,-1,1]
+    #1是向上飞 -1是向下飞
+    for i in range(0,50):
+        kinematics_state = client.simGetGroundTruthKinematics()
+        position = kinematics_state.position
+        label=draw(client,i,dir)
+        time.sleep(1)
+        #打标签
+        print(label)
+        # ser.write([0])
+        # time.sleep(0.001)
+        # if label<0.3:
+        #     ser.write([1])
+        #     time.sleep(0.001)
+        #     ser.write([0])
+        # else:
+        #     ser.write([2])
+        #     time.sleep(0.001)
+        #     ser.write([0])
+        nextHight=position.z_val-dir[i]*10
+        if n==1:
+            client.moveToZAsync(nextHight,10).join()
+        else:
+            time.sleep(1.5)
+            draw_mode2(client,i,dir)
+            print("done")
+        time.sleep(3)
+    client.reset()
+
